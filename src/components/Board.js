56846dd971068e8ab27dd0d9cc14600c501cd547
Roamdev <aroundthefog@gmail.com>
2025-04-19 16:19:55 +0400

    initial commit with tutorial base

---
+++ src/components/Board.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Square from './Square';
+import calculateWinner from '../utils/calculateWinner';
+
+export default function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? 'Winner: ' + winner
+    : 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {[0, 1, 2].map(i => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[3, 4, 5].map(i => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[6, 7, 8].map(i => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+    </>
+  );
+} 
